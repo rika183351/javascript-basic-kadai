5d052e1e4a8d008f1630ce088ca191f4b37a97d9
rika183351 <oer3regit1mahc2@gmail.com>
2025-03-15 10:40:45 +0900

    Delete kadai-021 directory

--- kadai-021/event.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('btn');
-const text = document.getElementById('text');
-
-setTimeout(() => {
-    btn.addEventListener('click', () => {
-        text.textContent = 'ボタンをクリックしました';
-        console.log(text);
-})}, 2000);
